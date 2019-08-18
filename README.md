c3ce900d4560c6fd5177865d7c86013e69820bcd
Patrick Venkat <patrick.venkat@gmail.com>
2019-08-18 11:55:25 -0400

    Initial push a working app

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # liri-node-app
+# 1. Problem Definition
+   # This node app solves 3 distinct problems. 
+   #a) Search spotify to return information about a song that is typed in the console.
+   #b) Search OMDB to return information about a movie that is typed in the console.
+   #c) Search either a song or a movie using a data file if no search input is provided
+# 2. High-level overview of how the app is organized
+   # This app is organized into the following sections 
+   #a) Setup block. This is where the packages are loaded, key and environment files are pulled in.
+   #b) Song search block. This is where the Spotify search call is made according to the node package and the return data is processed.
+   #b) Movie search block. This is where the OMDB search call is made using axios and the return data is processed.
+   #b) Read from file block. This is where the file is read and the data in the file is used to make a movie or a song search.
+# 3. Start-to-finish instructions on how to run the app
+   #a) In the console window, type in "node liri.js spotify-this-song 'followed by a song name'". This will return the information about the song you asked for. 
+   #b) In the console window, type in "node liri.js movie-this 'followed by a movie name'". This will return the information about the movie you asked for.
+   #c) In the console window, type in "node liri.js do-what-it-says". This will return the information about a movie or a song specified in a file. 
+   #d) In the console window, type in "node liri.js". Since no additional input was provided, the app will return the information about a movie or a song of it own choice.
+# 4. Screenshots, gifs or videos of the app functioning
+   #a) See github for it
+# 5. Link to a deployed version of the app
+   #a) Link to run the node app: 
+# 6. List the technologies used in the app
+   #a) Packages: Spotify Node API, OMBD node API, File System, Axios, Moment, dotenv.
+   #b) Files: .gitignore to store links to the key files. Key and env files to protect client ID and Secret from pushing up to GitHub.
+# 7. Your role in the app development
+   #a) Soup to nuts. Design, develop, test and deliver.
---
+++ liri.js
@@ -0,0 +1,202 @@
+// Start: Setup block************************************************
+require("dotenv").config();
+// Include the axios npm package
+let axios = require("axios");
+let fs = require("fs");
+// Include the spotify npm package
+let Spotify = require("node-spotify-api");
+let keys = require("./keys.js");
+let spotify = new Spotify(keys.spotify);
+// End: Setup block***************************************************
+
+// Start: Process the command line input for song search*************
+let songName = "";
+//Take input from the command line and display information about the song
+if (process.argv[2] == "spotify-this-song") {
+    for (i=3; i<process.argv.length; i++){
+        songName += process.argv[i] + " ";
+    }
+    //Call the search song function.
+    searchSpotify(songName);
+  }
+// End: Process the command line input for song search****************
+
+//Start: Song search function block of code**************************
+function searchSpotify(songName){
+  // Make a request to Spotify
+  spotify.search({type: 'track', query: songName})
+    .then(function (spotifyData) {
+      console.log(spotifyData.tracks.total);
+      if (spotifyData.tracks.total != 0){
+        console.log(`
+        **********Information is below for your song, ${spotifyData.tracks.items[0].name} *************
+        `)
+        let dataForFile = `
+        **********Information is below for your song, ${spotifyData.tracks.items[0].name} *************`
+        appendToLogFile(dataForFile);
+        // Loop through and print the artists from the array.
+        for (a=0; a<spotifyData.tracks.items[0].artists.length; a++){
+          console.log(`
+          Artist ${a+1}----------: ${spotifyData.tracks.items[0].artists[a].name}
+          `);
+          dataForFile =`
+          Artist ${a+1}----------: ${spotifyData.tracks.items[0].artists[a].name}`
+          appendToLogFile(dataForFile);
+        }
+        // Print the name, preview url and the album name.
+          console.log(`
+          The song's name---: ${spotifyData.tracks.items[0].name}
+          The preview URL---: ${spotifyData.tracks.items[0].preview_url}
+          The album name----: ${spotifyData.tracks.items[0].album.name}
+        `)
+          dataForFile =`
+          The song's name---: ${spotifyData.tracks.items[0].name}
+          The preview URL---: ${spotifyData.tracks.items[0].preview_url}
+          The album name----: ${spotifyData.tracks.items[0].album.name}
+        `
+        appendToLogFile(dataForFile);
+      } else{
+        let dataForFile = `
+      **********Information is below for your song, ${songName} *************
+        Sorry! Song not found!
+        `
+        appendToLogFile(dataForFile);
+      }
+      songName = "";
+    })
+    .catch(function(error) {
+      if (error.Response == "False") {
+        // The server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that 
+        // triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+      songName = "";
+  });
+}
+// End: Song search function block of code*****************************
+
+// Start: Process the command line input for movie search*************
+let movieName = "";
+//Take input from the command line and display information about the movie
+// Note: The movie can be any number of words and code will still be able to process it.
+if (process.argv[2] == "movie-this") {
+    for (i=3; i<process.argv.length; i++){
+        movieName += process.argv[i] + " ";
+    }
+    // Call the searchMovie function
+    searchMovie(movieName);
+  }
+// End: Process the command line input for movie search*************
+
+//Start: Movie search function block of code**************************
+function searchMovie(movieName){
+  // Then run a request with axios to the OMDB API with the movie specified
+  axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy")
+  .then(function(response) {    
+      if (response.data.Response == "True"){
+        console.log(`
+        **********Information is below for your movie, ${movieName} *************
+        The movie title is----------------------:${response.data.Title}
+        The Year the movie came out-------------:${response.data.Year}
+        The Rotten Tomatoes rating of the movie-:${response.data.Ratings[1].Value}
+        The Country where the movie was produced:${response.data.Country}
+        The Language of the movie---------------:${response.data.Language}
+        The Plot of the movie-------------------:${response.data.Plot}
+        The Actors in the movie-----------------:${response.data.Actors}
+        The IMDB rating of the movie------------:${response.data.imdbRating}
+        The response of the call----------------:${response.data.Response}
+        `);
+        let dataForFile = `
+        **********Information is below for your movie, ${movieName} *************
+          The movie title is----------------------:${response.data.Title}
+          The Year the movie came out-------------:${response.data.Year}
+          The Rotten Tomatoes rating of the movie-:${response.data.Ratings[1].Value}
+          The Country where the movie was produced:${response.data.Country}
+          The Language of the movie---------------:${response.data.Language}
+          The Plot of the movie-------------------:${response.data.Plot}
+          The Actors in the movie-----------------:${response.data.Actors}
+          The IMDB rating of the movie------------:${response.data.imdbRating}
+        `
+        appendToLogFile(dataForFile);
+      } else{
+        let dataForFile = `
+        **********Information is below for your movie, ${movieName} *************
+          Sorry! ${response.data.Error}
+        `
+        appendToLogFile(dataForFile);
+      }
+      movieName = "";
+    })
+    .catch(function(error) {
+      if (error.Response == "True") {
+        // The server responded with a status code
+        //  that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that 
+          // triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+        movieName = "";
+  });
+}
+// End: Movie search function block of code**************************
+
+// Start: Process the command line input for do-what-it-says*************
+if (process.argv[2] == "do-what-it-says"){
+    // Call the searchMovie function
+    doWhatItSays();
+  }
+// End: Process the command line input for do-what-it-says****************
+
+// Start: File read and processing function block of code*****************
+function doWhatItSays(){
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    // Log the error to the console in case there is an error.
+    if (error) {
+      return console.log(error);
+    }
+    // Then split it by commas to make it more readable and create an array
+    var dataArr = data.split(",");
+    // Loop through the array to execute the request
+    if (dataArr[0] == "spotify-this-song"){
+      searchSpotify(dataArr[1]);
+    }
+  });
+}
+// End: File read and processing function block of code*******************
+
+// Start: Write to log file processing function block of code*************
+function appendToLogFile(textInput){
+  fs.appendFile("liriLog.txt", textInput, function(err) {
+    // If an error was experienced we will log it.
+    if (err) {
+      console.log(err);
+    }
+    // If no error is experienced, we'll log the phrase "Content Added to Log File" to our node console.
+    else {
+      console.log("Content Added to Log File!");
+    }
+  });
+}
+// End: Write to log file processing function block of code************
